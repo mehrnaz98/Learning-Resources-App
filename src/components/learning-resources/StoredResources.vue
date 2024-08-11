f126765b5035070cd1b0d9f4960ee5b296cfce6a
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-11 22:15:35 +0330

    Update StoredResources.vue
    Add the props resources that it will get from the App.vue

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,15 @@
+<template>
+  <learning-resource
+    v-for="res in storedResources"
+    :key="res.id"
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+  ></learning-resource>
+</template>
+
+<script>
+export default {
+  props: ['resources'],
+};
+</script>
