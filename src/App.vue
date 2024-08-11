e88d436996cb5e396bcf611236373224a0d11740
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-11 23:59:20 +0330

    Pass the storedResources value through the resources prop to StoredResources

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <ul>
-    <learning-resource
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    ></learning-resource>
-  </ul>
+  <StoredResources :resources="storedResources"></StoredResources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
-    LearningResource,
+    StoredResources,
   },
   data() {
     return {
