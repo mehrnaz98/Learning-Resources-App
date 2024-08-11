66b4412cef2e5e4b67cc1d0b9eaa443089f695ac
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-11 21:05:47 +0330

    Update LearningResource.vue

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+      <p></p>
+      <nav>
+        <a href="">View Resource</a>
+      </nav>
+    </div>
+  </li>
+</template>
