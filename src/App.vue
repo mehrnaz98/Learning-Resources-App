8cb9e1b8148f9d8e34f4817e2840ea0dd5b14094
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-11 21:14:44 +0330

    Bind and pass in the props

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <learning-resource
       v-for="res in storedResources"
       :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
     ></learning-resource>
   </ul>
 </template>
