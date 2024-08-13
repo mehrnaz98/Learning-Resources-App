3e63b9fdfaed83351312a6d8f063f95d3728ac5f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-13 22:31:01 +0330

    Add the storedResources data to TheResources

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -20,6 +20,20 @@ export default {
   data() {
     return {
       selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
     };
   },
   methods: {
