853543ee18767a33f8769ca7b893d40e0237cabb
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-13 22:37:26 +0330

    Import TheResources locally and register it in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <the-header title="RememberMe"></the-header>
-  <StoredResources :resources="storedResources"></StoredResources>
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
     TheHeader,
+    TheResources,
   },
 };
 </script>
