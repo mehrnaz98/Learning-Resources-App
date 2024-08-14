217307217bc9f61b95d94350ca380059f47ed371
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-14 23:11:45 +0330

    Add a form

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,10 @@
 <template>
-  <h2>Add Resources</h2>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" />
+      </div>
+    </form>
+  </base-card>
 </template>
