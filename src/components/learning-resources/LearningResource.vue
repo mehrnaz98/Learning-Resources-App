30940c0e4ee14f2ad55523eb4d98fdf7fda6ed11
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-15 17:39:15 +0330

    Get id as a prop and send the resource id that must be deleted to deleteResource that was injected

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,9 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <base-button mode="flat">Delete</base-button>
+        <base-button mode="flat" @click="deleteResource(id)"
+          >Delete</base-button
+        >
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -17,7 +19,7 @@
 import BaseButton from '../UI/BaseButton.vue';
 export default {
   components: { BaseButton },
-  props: ['title', 'description', 'link'],
+  props: ['id', 'title', 'description', 'link'],
   inject: ['deleteResource'],
 };
 </script>
