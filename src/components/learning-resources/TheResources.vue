3734da217d0d16eef2252a91a5a4d15c2d4e5f44
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-15 17:15:03 +0330

    Provide the removeResource
    It is the resource that will be deleted.

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -44,6 +44,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.deleteResource,
     };
   },
   computed: {
